e32f208ebdebfe0c6e785c90cb232b50e0db09eb
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-05-06 15:57:55 -0700

    commit tree ac609cfef79232ddec456f33ef39bdb7cdd79e6c

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newArray= [...cats];
+    newArray.push(name);
+    return newArray;
+}
+
+function prependCat(name) {
+    const newArray= [...cats];
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat() {
+    const newArray= [...cats];
+    newArray.pop();
+    return newArray;
+}
+function removeFirstCat() {
+    const newArray= [...cats];
+    newArray.shift();
+    return newArray;
+}
+
